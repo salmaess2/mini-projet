dbbb77832db3a0f1156086e33e94e5981342b01f
salmaess2 <essahis13@gmail.com>
2025-09-24 18:47:41 +0200

    init miniproject

---
+++ miniProjet1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+ char nom[20];
+ char tele[20];
+  
+  void ajouter() {
+    printf("le nom: ");
+    scanf("%s",&nom);
+    printf("le telephone: ");
+    scanf("%s",&tele);
+    printf("%s",)
+   }
+  void afficher() {
+    int i,j;
+    int tab [2][50]={};
+    for(i=0; i<50; i++){
+      for(j=0; j<50; j++){
+       printf("%d",tab[2][50]);
+      }
+    }
+   }
+void rechercher(){
+    char nomUtilisateur;
+ printf("entrer un nom: ");
+ scanf("%s",nomUtilisateur);
+ if(nom == nomUtilisateur)
+   printf(tab)
+}
+int main() {
+    ajouter();
+    afficher();
+    recherher();
+    supprumer();
+    quitter();
+      switch 
+        case 1 :
+         printf("ajouter un contact");
+         break;
+        case 2:        
+    return 0;
+}
